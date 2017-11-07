fc5cae655430bb84cf971546a321ffc9778dedb0
Nicholas Reichert <nic13reich@gmail.com>
2017-11-07 18:26:56 -0500

    Added code for a servo on the MechaBot and improved Mechanum code.

--- TeamCode/src/main/java/robotx/modules/MechaBot.java
+++ TeamCode/src/main/java/robotx/modules/MechaBot.java
@@ -1,8 +1,9 @@
-package robotx.modules;
+ package robotx.modules;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import robotx.libraries.XModule;
 
@@ -18,6 +19,8 @@ public class MechaBot extends XModule {
     DcMotor backLeft;
     DcMotor backRight;
 
+    Servo armServo;
+
     public MechaBot(OpMode op) {super(op);}
 
     public void init(){
@@ -27,15 +30,31 @@ public class MechaBot extends XModule {
         backLeft = opMode.hardwareMap.dcMotor.get("backLeft");
         backRight = opMode.hardwareMap.dcMotor.get("backRight");
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        armServo = opMode.hardwareMap.servo.get("armServo");
     }
     public void loop() {
         float forwardBackAxis = xGamepad1().left_stick_y;
         float leftRightAxis = xGamepad1().left_stick_x;
         float spinRight = xGamepad1().right_trigger;
         float spinLeft = xGamepad1().left_trigger;
+        float spin = spinRight - spinLeft;
 
+        boolean aPressed = xGamepad1().a.wasPressed();
+
+
+        if (aPressed == true)
+        {
+            armServo.setPosition(0.4);
+        }
+        else
+        {
+            armServo.setPosition(0.6);
+        }
+         
         //Sets motion to back and forth or left and right depending on which joystick value is greater
-        if (Math.abs(forwardBackAxis) > Math.abs(leftRightAxis)) {
+        // We should not use a casewise method for this.
+        /*if (Math.abs(forwardBackAxis) > Math.abs(leftRightAxis)) {
             frontRight.setPower(forwardBackAxis);
             frontLeft.setPower(forwardBackAxis);
             backLeft.setPower(forwardBackAxis);
@@ -45,7 +64,12 @@ public class MechaBot extends XModule {
             backLeft.setPower(-leftRightAxis);
             frontRight.setPower(-leftRightAxis);
             backRight.setPower(leftRightAxis);
-        }
+        }*/
+
+        frontRight.setPower(forwardBackAxis + leftRightAxis + spin);
+        frontLeft.setPower(forwardBackAxis - leftRightAxis - spin);
+        backRight.setPower(forwardBackAxis - leftRightAxis + spin);
+        backLeft.setPower(forwardBackAxis + leftRightAxis - spin);
 
         /*frontLeft.setPower(-spinLeft);
         backLeft.setPower(-spinLeft);
