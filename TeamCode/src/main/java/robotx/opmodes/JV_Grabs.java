2885cfae58a0a47b8f2794a38b0f29fc194d7ef7
Adithya Bharathan <BharathanA11698@stxavier.org>
2018-01-05 17:14:48 -0500

    Set buttons x,y, and b to close, open, and grab the glyph in an ideal position.

---
+++ TeamCode/src/main/java/robotx/opmodes/JV_Grabs.java
@@ -0,0 +1,69 @@
+package robotx.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import robotx.libraries.XGamepad;
+import robotx.libraries.XOpMode;
+
+
+/**
+ * Created by Adithya on 1/5/2018.
+ */
+@TeleOp(name = "JV_Grabs", group = "Default")
+
+public class JV_Grabs extends XOpMode {
+    Servo leftServo;
+    Servo rightServo;
+
+    double Power3 = 0.5;
+
+    @Override
+    public void init()
+    {
+        super.init();
+        rightServo = hardwareMap.servo.get("RightServo");
+        leftServo = hardwareMap.servo.get("LeftServo")
+    }
+
+    @Override
+    public void init_loop() {
+        super.init_loop();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop()
+    {
+        super.loop();
+        if (xGamepad2.x.isDown())       //x will open the claw all the way
+        {
+            rightServo.setPosition(0.85);
+            leftServo.setPosition(0.9);
+        }
+
+        else if (xGamepad2.y.isDown()) //y will set it to the optimal, middle position
+        {
+            rightServo.setPosition(0.4);
+            leftServo.setPosition(0.5);
+
+        }
+
+        else if (xGamepad2.b.isDown())  //b will close the claw all the way
+        {
+            rightServo.setPosition(0.0);
+            leftServo.setPosition(0.9);
+        }
+    }
+    
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
