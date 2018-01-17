97d14f9620d16aba6d454f8fb00b18d6e3074bf5
Lego <kushdalal007@gmail.com>
2018-01-17 15:34:30 -0500

    Strafing was reversed. now fixed

--- TeamCode/src/main/java/robotx/libraries/OmniDriveSystem.java
+++ TeamCode/src/main/java/robotx/libraries/OmniDriveSystem.java
@@ -71,7 +71,7 @@ public abstract class OmniDriveSystem extends XModule {
 
     public void loop() {
         if (controlsEnabled) {
-            double xPow = controlRamp(xGamepad1().left_stick_x);
+            double xPow = controlRamp(-xGamepad1().left_stick_x);
             double yPow = controlRamp(-xGamepad1().left_stick_y); // Negate the left stick value because negative is up.
             double rotPow = controlRamp(xGamepad1().right_stick_x);
 
