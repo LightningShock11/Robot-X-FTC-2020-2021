f089352c9d2dfa2fa74004040a7051d18c33f325
Ben Sabo <sabob11057@stxavier.org>
2018-01-23 18:22:31 -0500

    Fixed issue where boolean was always set to false and fixed the autonomous where the robot would knock off the opposite gen that is was supposed to

--- TeamCode/src/main/java/robotx/modules/JewelColor.java
+++ TeamCode/src/main/java/robotx/modules/JewelColor.java
@@ -85,20 +85,20 @@ public class JewelColor extends XModule {
     public void knockOffRightGem() {
         lowerArm();
         sleep(500);
-        autonomousMovement.pointTurnRight(15);
+        autonomousMovement.pointTurnLeft(15);
         sleep(500);
         raiseArm();
         sleep(500);
-        autonomousMovement.pointTurnLeft(15);
+        autonomousMovement.pointTurnRight(15);
     }
     public void knockOffLeftGem() {
         lowerArm();
         sleep(500);
-        autonomousMovement.pointTurnLeft(15);
+        autonomousMovement.pointTurnRight(15);
         sleep(500);
         raiseArm();
         sleep(500);
-        autonomousMovement.pointTurnRight(15);
+        autonomousMovement.pointTurnLeft(15);
     }
 
     public void knockOffRedGem() {
