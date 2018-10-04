c581cd35a7a898a62228c1c5098fdfeb3c6ff322
Ben Sabo <sabob11057@stxavier.org>
2018-10-04 19:06:10 -0400

    Updated color sensor sorting program by tuning the servo positions and setting the initial servo position during init.

--- TeamCode/src/main/java/robotx/modules/SortingProtoArm.java
+++ TeamCode/src/main/java/robotx/modules/SortingProtoArm.java
@@ -23,28 +23,31 @@ public class SortingProtoArm extends XModule {
         //initialize color sensor
         sortingColor = opMode.hardwareMap.colorSensor.get("sortingColor");
         sortingColor.setI2cAddress(I2cAddr.create7bit(0x39)); // All REV color sensors use this address
-        sortingColor.enableLed(true);
+        sortingColor.enableLed(false);
+
         opMode.telemetry.addLine("Color sensor is online");
+
+        protoServo.setPosition(0.85);
+        //Open servo during init
 
     }
 
     public void loop(){
         //constantly test the below
 
-        if (sortingColor.red() > 220 && sortingColor.green() > 220 && sortingColor.blue() > 220) {
-            //check if the color is close enough to white
+            if (sortingColor.red() > 240 && sortingColor.green() > 240 && sortingColor.blue() > 240) {
+                //check if the color is close enough to white
 
-            protoServo.setPosition(0.8); //set the servo the drop in white
+                protoServo.setPosition(0.6); //set the servo the drop in white
 
-        }else if (xGamepad1().b.isDown()) {
-            //manual override for testing purposes
+            } else if (xGamepad1().b.isDown()) {
+                //manual override for testing purposes
 
-            protoServo.setPosition(0.8);
+                protoServo.setPosition(0.6);
 
-        }else{
-            //keep the servo open
-            protoServo.setPosition(1.0); //set the servo to open for gold cubes
+            } else {
+                //keep the servo open
+                protoServo.setPosition(0.85); //set the servo to open for gold cubes
+            }
         }
-
     }
-}
