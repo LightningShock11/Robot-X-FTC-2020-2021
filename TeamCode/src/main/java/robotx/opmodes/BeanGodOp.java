61b205f4726cd20699b03bcab5176842c7973f04
Lego <kushdalal007@gmail.com>
2018-10-22 16:17:20 -0400

    Added the sorting arm code to the main opmode (beanGodOp = Main Opmode for robot control)

--- TeamCode/src/main/java/robotx/opmodes/BeanGodOp.java
+++ TeamCode/src/main/java/robotx/opmodes/BeanGodOp.java
@@ -3,17 +3,23 @@ package robotx.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import robotx.libraries.XOpMode;
+import robotx.modules.SortingProtoArm;
 import robotx.modules.TwoMotorDrive;
 
 @TeleOp(name = "BeanGodOp", group = "Testing")
 public class BeanGodOp extends XOpMode {
     TwoMotorDrive twoMotorDrive;
+    SortingProtoArm sortingProtoArm;
+
 
     public void initModules(){
         super.initModules();
 
         twoMotorDrive = new TwoMotorDrive(this);
         activeModules.add(twoMotorDrive);
+
+        sortingProtoArm = new SortingProtoArm(this);
+        activeModules.add(sortingProtoArm);
     }
     public void init(){super.init();}
 
