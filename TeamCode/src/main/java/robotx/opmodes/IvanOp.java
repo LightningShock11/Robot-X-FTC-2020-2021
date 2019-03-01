8a3399aeda77806d4e99f899510c05b516244324
Robot-X team member <robotx4969@gmail.com>
2019-03-01 16:57:10 -0500

    Edited IvanOp

--- TeamCode/src/main/java/robotx/opmodes/IvanOp.java
+++ TeamCode/src/main/java/robotx/opmodes/IvanOp.java
@@ -12,25 +12,16 @@ import robotx.modules.RLdrive;
 import robotx.modules.SortingProtoArm;
 import robotx.modules.XSweeper;
 
-@TeleOp(name = "IvanOp", group = "Testing")
+@TeleOp(name = "IvanOp", group = "Worlds")
 public class IvanOp extends XOpMode {
-    //TwoMotorDrive twoMotorDrive;
-    //OneStickDrive oneStickDrive;
     MechanumDriveNoLag mechanumDriveNoLag;
-    SortingProtoArm sortingProtoArm;
-    LiftSystemXY liftSystemXY;
     XSweeper xSweeper;
-    DumpingBucket dumpingBucket;
 
     public void initModules(){
         super.initModules();
 
         mechanumDriveNoLag = new MechanumDriveNoLag(this); //Driving system
         activeModules.add(mechanumDriveNoLag);
-
-        liftSystemXY = new LiftSystemXY(this); //lift system
-        activeModules.add(liftSystemXY);
-
     }
     public void init(){super.init();}
 }
