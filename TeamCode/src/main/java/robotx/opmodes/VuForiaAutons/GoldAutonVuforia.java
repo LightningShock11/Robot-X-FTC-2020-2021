feb8aec7375f2ba0e6783fd66a34cf18f0383643
Lego <bobbymcalpine@gmail.com>
2019-03-07 16:56:51 -0500

    Worked on auton with vuforia, may work, still needs testing

--- TeamCode/src/main/java/robotx/opmodes/VuForiaAutons/GoldAutonVuforia.java
+++ TeamCode/src/main/java/robotx/opmodes/VuForiaAutons/GoldAutonVuforia.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import robotx.libraries.AutonomousMovement;
+import robotx.libraries.VuForiaObjectDetection;
 import robotx.libraries.XLinearOpMode;
 import robotx.modules.DumpingBucket;
 import robotx.modules.LiftSystemXY;
@@ -23,9 +24,7 @@ public class GoldAutonVuforia extends XLinearOpMode {
     TwoWheelAutonIMU sensors;
     TwoMotorDrive twoMotorDrive;
     LiftSystemXY liftSystemXY;
-    MineralColor mineralColor;
-    DumpingBucket dumpingBucket;
-    XSweeper xSweeper;
+    VuForiaObjectDetection scanner;
 
 
     public void runOpMode() {
@@ -35,11 +34,11 @@ public class GoldAutonVuforia extends XLinearOpMode {
         telemetry.addData("Stage", "Init: Gold Auton");
         this.updateTelemetry(telemetry);
 
+        scanner.init();
+
         twoMotorDrive = new TwoMotorDrive(this);
         twoMotorDrive.init();
 
-        dumpingBucket = new DumpingBucket(this);
-        dumpingBucket.init();
 
         sensors = new TwoWheelAutonIMU(this);
         sensors.init();
@@ -49,14 +48,6 @@ public class GoldAutonVuforia extends XLinearOpMode {
 
         movement = new AutonomousMovement(this, sensors, twoMotorDrive);
         movement.init();
-
-        mineralColor = new MineralColor(this);
-        mineralColor.movement = movement;
-        mineralColor.liftSystemXY = liftSystemXY;
-        mineralColor.init();
-
-        xSweeper = new XSweeper(this);
-        xSweeper.init();
 
         sensors.leftMotor = twoMotorDrive.leftMotor;
         sensors.rightMotor = twoMotorDrive.rightMotor;
@@ -76,32 +67,20 @@ public class GoldAutonVuforia extends XLinearOpMode {
         twoMotorDrive.start();
         twoMotorDrive.rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         twoMotorDrive.leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        scanner.start();
+        scanner.runOpMode();
 
         /////////////////////Movement///////////////////////
 
-        liftSystemXY.extendY(1300);
-        sleep(150);
-        movement.pointTurnRight(30);
-        sleep(100);
-        goForward(1.0, 250);
-        sleep(100);
-        movement.pointTurnLeft(30);
+
         sleep(250);
 
         //-----------Dehanging complete-----------\\
 
-        liftSystemXY.extendX(1325);
-        mineralColor.DetectGold();
-        sleep(100);
-        mineralColor.knockMineral();
-        liftSystemXY.retractX();
-        goForward(1.0, 700);
-        movement.pointTurnRight(90);
-        goBackward(1.0, 900);
-        dumpingBucket.autoDump();
-        sleep(100);
-        goForward(1.0, 2000);
+        if(scanner.isCenter){
+            goForward(1.0, 1000);
+        }
+
         stopDriving();
         twoMotorDrive.stop();
         movement.stop();
