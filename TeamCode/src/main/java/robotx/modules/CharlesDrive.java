507b4d6fea028bb4993d45a8c7c257530d913928
DESKTOP-DM8CSLJ\Ben Sabo <sabob11057@stxavier.org>
2019-09-30 17:12:57 -0400

    Tried to work on driving code. Still doesnt worl

--- TeamCode/src/main/java/robotx/modules/CharlesDrive.java
+++ TeamCode/src/main/java/robotx/modules/CharlesDrive.java
@@ -23,7 +23,7 @@ public class CharlesDrive extends XModule {
         frontLeft = opMode.hardwareMap.dcMotor.get("frontLeft");
         frontRight = opMode.hardwareMap.dcMotor.get("frontRight");
         backRight = opMode.hardwareMap.dcMotor.get("backRight");
-        //backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft = opMode.hardwareMap.dcMotor.get("backLeft");
         //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
     }
@@ -36,50 +36,52 @@ public class CharlesDrive extends XModule {
 
         ////////////////////Cartesian to Polar Conversions/////////////////////////////
 
-        if(x > 0 && y > 0){
+        if(x > 0 && y > 0){ //Quadrant 1
             theta = Math.toDegrees(Math.atan(y/x)); //theta = the inverse tangent of y/x
             r = Math.sqrt(Math.pow(x,2.0) + Math.pow(y,2.0)); //r = the square root of x^2 + y^2 (Pythagorean Theorem)
         }
-        else if(x < 0 && y > 0){
-            theta = (180 - Math.toDegrees(Math.atan(y/x)));
+        /*else if(x < 0 && y > 0){ //Quadrant 2
+            theta = (180 + Math.toDegrees(Math.atan(y/x)));
             r = Math.sqrt(Math.pow(x,2.0) + Math.pow(y,2.0));
         }
-        else if(x < 0 && y < 0){
+        else if(x < 0 && y < 0){ //Quadrant 3
             theta = (180 + Math.toDegrees(Math.atan(y/x)));
         r = Math.sqrt(Math.pow(x,2.0) + Math.pow(y,2.0));
         }
-        else if(x < 0 && y > 0){
+        else if(x > 0 && y < 0){ //Quadrant 4
             theta = (360 - Math.toDegrees(Math.atan(y/x)));
             r = Math.sqrt(Math.pow(x,2.0) + Math.pow(y,2.0));
         }
-
+*/
         ////////////////////Applying conversions to motors////////////////////////////
 
-        if (0<theta && theta<90){ //Quadrant 1 code
+        //  1/45 = 0.022222
+
+        if (x > 0 && y > 0){ //Quadrant 1 code
             frontLeft.setPower(1*r);
             backRight.setPower(1*r);
-            frontRight.setPower((((1/45)*theta)-1)*r);
-            backLeft.setPower((((1/45)*theta)-1)*r);
+            frontRight.setPower((((.02222)*theta)-1)*r);
+            backLeft.setPower((((.02222)*theta)-1)*r);
         }
-        else if (90<theta && theta<180){ //quadrant 2 code
-            frontLeft.setPower((((-1/45)*theta)+3)*r);
-            backRight.setPower((((-1/45)*theta)+3)*r);
+       /* else if (x < 0 && y > 0){ //quadrant 2 code
+            frontLeft.setPower((((-.02222)*theta)+3)*r);
+            backRight.setPower((((-.02222)*theta)+3)*r);
             frontRight.setPower(1*r);
             backLeft.setPower(1*r);
         }
-        else if (-180<theta && theta<-90){ //quadrant 3 code
+        else if (x < 0 && y < 0){ //quadrant 3 code
             frontLeft.setPower(-1*r);
             backRight.setPower(-1*r);
-            frontRight.setPower((((-1/45)*theta)-3)*r);
-            backLeft.setPower((((-1/45)*theta)-3)*r);
+            frontRight.setPower((((-.02222)*theta)-3)*r);
+            backLeft.setPower((((-.02222)*theta)-3)*r);
         }
-        else if (-90<theta && theta<0){ //quadrant 4 code
-            frontLeft.setPower((((1/45)*theta)+1)*r);
-            backRight.setPower((((1/45)*theta)+1)*r);
+        else if (x > 0 && y < 0){ //quadrant 4 code
+            frontLeft.setPower((((.02222)*theta)+1)*r);
+            backRight.setPower((((.02222)*theta)+1)*r);
             frontRight.setPower(-1*r);
             backLeft.setPower(-1*r);
         }
-
+*/
         else{
             frontLeft.setPower(0);
             backRight.setPower(0);
