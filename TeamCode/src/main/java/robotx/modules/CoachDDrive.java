c85a4560e636f4c083ec96c6c61d77b59fb77fea
Ben Sabo <sabob11057@stxavier.org>
2019-10-11 13:26:29 -0400

    Added rotation into the driving code. Still needs testing

--- TeamCode/src/main/java/robotx/modules/CoachDDrive.java
+++ TeamCode/src/main/java/robotx/modules/CoachDDrive.java
@@ -17,6 +17,7 @@ public class CoachDDrive extends XModule {
     double x;
     double y;
     double s;
+    double r;
 
     public void init(){
         frontLeft = opMode.hardwareMap.dcMotor.get("frontLeft");
@@ -31,12 +32,13 @@ public class CoachDDrive extends XModule {
     public void loop(){
         x = xGamepad1().left_stick_x;
         y = xGamepad1().left_stick_y;
-        s = ((Math.max(Math.abs(x), Math.abs(y)))*(Math.max(Math.abs(x), Math.abs(y))))/((x*x)+(y*y));
+        r = xGamepad1().right_stick_x;
+        s = ((Math.max(Math.abs(x), Math.max(Math.abs(y), Math.abs(r))))*(Math.max(Math.abs(x), Math.max(Math.abs(y), Math.abs(r)))))/((x*x)+(y*y)+(r*r));
 
-        frontLeft.setPower((y-x)*(s));
-        backRight.setPower((y-x)*(s));
+        frontLeft.setPower((y-x+r)*(s));
+        backRight.setPower((y-x-r)*(s));
 
-        frontRight.setPower((y+x)*(s));
-        backLeft.setPower((y+x)*(s));
+        frontRight.setPower((y+x-r)*(s));
+        backLeft.setPower((y+x+r)*(s));
     }
 }
