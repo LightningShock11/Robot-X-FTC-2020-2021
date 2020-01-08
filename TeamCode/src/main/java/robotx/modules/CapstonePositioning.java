ecef70a96dacea23fa14ef7aa5f0c4af85555139
Sam <mcdowells13111@stxavier.org>
2020-01-08 15:10:56 -0500

    Created code for capstone positioning

---
+++ TeamCode/src/main/java/robotx/modules/CapstonePositioning.java
@@ -0,0 +1,30 @@
+package robotx.modules;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import robotx.libraries.XModule;
+
+import static android.os.Build.VERSION_CODES.P;
+
+public class CapstonePositioning extends XModule {
+
+    public Servo capstoneServo;
+
+    public CapstonePositioning(OpMode op){super(op);}
+
+    public void init () {
+        capstoneServo = opMode.hardwareMap.servo.get("clawServo");
+        capstoneServo.setPosition(0);
+    }
+    public void loop () {
+
+        if(xGamepad2().x.wasPressed()) {
+            if(capstoneServo.getPosition() == 0) {
+                capstoneServo.setPosition(0.5);
+            } else {
+                capstoneServo.setPosition(0);
+            }
+        }
+    }
+}
